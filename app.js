2485da2d3b8546ced9befea821274f4de7610356
JIHYUNHA <humm223@gmail.com>
2020-12-03 17:52:56 +0900

    feat: Set database config

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
+require('./config/db');
+
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 
@@ -11,6 +15,10 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
